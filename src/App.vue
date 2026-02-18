41cf6ccbb6c66ef2980fa5fcecac4f64f02fba19
FullDevJuan <juandev003@gmail.com>
2026-02-18 10:03:07 -0500

    componente para mostrar info de users con v-for

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import UserCard from "./components/UserCard.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    UserCard,
+  },
+};
+</script>
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld
+      msg="Welcome to my first interaction with Vue.js 2"
+    ></HelloWorld>
+    <UserCard></UserCard>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: monospace;
 }
 </style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-for="user in users" :key="user.id" class="user-card">
+      <h2>User info</h2>
+      <p>Name: {{ user.name }}</p>
+      <p>Username: {{ user.username }}</p>
+      <p>Email: {{ user.email }}</p>
+      <p>
+        Address: {{ user.address.street }}, {{ user.address.suite }},
+        {{ user.address.city }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async mounted() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    this.users = users;
+  },
+};
+</script>
+
+<style>
+.user-card {
+  border: 1px solid #ccc;
+  padding: 16px;
+  border-radius: 8px;
+  max-width: 300px;
+}
+</style>
